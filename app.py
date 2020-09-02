106243c8d8dbfb6ab127729acd880b7ae76dc872
Rituraj Ojha <rituraj0480@gmail.com>
2020-09-02 21:03:39 +0530

    "last"

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from new import new_blueprint
 from top import top_blueprint
 
 app = Flask(__name__)
+
 app.register_blueprint(hot_blueprint)
 app.register_blueprint(new_blueprint)
 app.register_blueprint(top_blueprint)
